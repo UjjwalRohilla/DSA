75316790d41e36f545643a77a928fe0fdd7156e6
Ujjwalrohilla <ujjwalrohilla22i@gmail.com>
2023-01-09 20:46:23 +0530

    Questions

--- src/JavaCourse/BinarySeaarchQuestions/QuestionThreeViceVersa.java
+++ src/JavaCourse/BinarySeaarchQuestions/QuestionThreeViceVersa.java
@@ -1,24 +1,21 @@
 package JavaCourse.BinarySeaarchQuestions;
 
-import java.util.Scanner;
-
-public class QuestionThree {
+public class QuestionThreeViceVersa {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         char[] arr = {'c','f','j'};
-        char target = 'l';
+        char target = 'f';
         char ans = binarySearch(arr,target);
         System.out.println(ans);
     }
-    static char binarySearch(char[] arr,char target){
+    static char binarySearch(char[] letters,char target){
         int start = 0;
-        int end = arr.length-1;
+        int end = letters.length-1;
         while(start<=end){
             int mid = start+(end-start)/2;
-            if (arr[mid]>target) {
-                end = mid-1;
-            }else {
+            if (letters[mid]<target) {
                 start = mid+1;
+            }else {
+                end = mid-1;
             }
 //            if (letters[mid]<target) {
 //                start = mid+1;
@@ -26,6 +23,6 @@ public class QuestionThree {
 //                end = mid-1;
 //            }
         }
-        return arr[start%arr.length];
+        return letters[end%letters.length];
     }
 }
